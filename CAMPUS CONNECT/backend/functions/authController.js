59c8b855567006011d0100b75045f3b4c1b44a89
Rajat Latwal <rajatlatwal0@gmail.com>
2025-09-27 22:37:07 +0530

    Update authController.js

---
+++ CAMPUS CONNECT/backend/functions/authController.js
@@ -0,0 +1,5 @@
+// TODO: Register Firebase functions
+exports.auth = require("./authController");
+exports.events = require("./eventsController");
+exports.clubs = require("./clubsController");
+exports.notifications = require("./notificationsController");
