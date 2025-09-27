0a275f9f87f314a85e9772333dc325ced0ef3e45
Rajat Latwal <rajatlatwal0@gmail.com>
2025-09-27 22:39:00 +0530

    Update clubsController.js

---
+++ CAMPUS CONNECT/backend/functions/clubsController.js
@@ -0,0 +1,9 @@
+exports.getAllClubs = (req, res) => {
+  // TODO: fetch all clubs
+  res.json({ clubs: [] });
+};
+
+exports.createClub = (req, res) => {
+  // TODO: add new club
+  res.json({ msg: "Club created" });
+};
