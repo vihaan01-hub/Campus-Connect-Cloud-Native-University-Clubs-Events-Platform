782b2d87ab22a1d212ed1a1c9fb0d31d484b2a73
Rajat Latwal <rajatlatwal0@gmail.com>
2025-09-27 22:38:28 +0530

    Update authController.js

--- CAMPUS CONNECT/backend/functions/authController.js
+++ CAMPUS CONNECT/backend/functions/authController.js
@@ -1,5 +1,9 @@
-// TODO: Register Firebase functions
-exports.auth = require("./authController");
-exports.events = require("./eventsController");
-exports.clubs = require("./clubsController");
-exports.notifications = require("./notificationsController");
+exports.createUserProfile = (req, res) => {
+  // TODO: validate request
+  res.json({ msg: "User profile created" });
+};
+
+exports.loginUser = (req, res) => {
+  // TODO: login user
+  res.json({ msg: "User logged in" });
+};
