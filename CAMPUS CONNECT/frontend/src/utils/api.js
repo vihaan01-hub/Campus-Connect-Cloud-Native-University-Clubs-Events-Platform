ee08831f63044a3ff961d6809edc35ac68c71805
Rajat Latwal <rajatlatwal0@gmail.com>
2025-09-27 22:22:21 +0530

    Update api.js

---
+++ CAMPUS CONNECT/frontend/src/utils/api.js
@@ -0,0 +1,14 @@
+export const loginUser = async (credentials) => {
+  // TODO: POST /auth/login
+  return {};
+};
+
+export const fetchEvents = async () => {
+  // TODO: GET /events
+  return [];
+};
+
+export const fetchClubs = async () => {
+  // TODO: GET /clubs
+  return [];
+};
